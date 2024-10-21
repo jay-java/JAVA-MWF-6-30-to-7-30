be8c117d2b74affc38e454b90aa7da902d28a1ae
jay-java <jayprajapati.tops@gmail.com>
2024-10-21 19:49:10 +0530

    first session

---
+++ CoreJAVA/src/core/P001_First.java
@@ -0,0 +1,13 @@
+package core;
+//3 streams
+
+//1.in
+//2.out
+//3.err
+public class P001_First {
+	public static void main(String[] args) {
+		System.out.println("hello java");
+		System.out.println("hello again");
+		System.err.println("hello java again");
+	}
+}
