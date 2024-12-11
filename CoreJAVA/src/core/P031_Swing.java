c8367a733d6144d113e2bacb3e84a400c8b25ce1
jay-java <jayprajapati.tops@gmail.com>
2024-12-11 19:13:13 +0530

    swing CRUD

--- CoreJAVA/src/core/P031_Swing.java
+++ CoreJAVA/src/core/P031_Swing.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.JDBCType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -11,6 +12,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+class DataNotFound {
+	public DataNotFound() {
+		JFrame fr = new JFrame("Alert");
+		fr.setVisible(true);
+		fr.setSize(200, 150);
+		fr.setLayout(null);
+
+		JLabel l1 = new JLabel("Data not found");
+		l1.setBounds(50, 75, 120, 20);
+		fr.add(l1);
+	}
+}
 
 class SwingDemo implements ActionListener {
 	JLabel l1, l2, l3, l4;
@@ -70,14 +84,13 @@ class SwingDemo implements ActionListener {
 		b4 = new JButton("delete");
 		b4.setBounds(230, 280, 120, 20);
 		fr.add(b4);
-		
+
 		b1.addActionListener(this);
 		b2.addActionListener(this);
 		b3.addActionListener(this);
 		b4.addActionListener(this);
 	}
-	
-	
+
 	public static Connection createConnection() {
 		Connection conn = null;
 		try {
@@ -94,23 +107,23 @@ class SwingDemo implements ActionListener {
 		if (e.getSource() == b1) {
 			System.out.println("insert button clicked");
 			int id = Integer.parseInt(t1.getText());
-			String name= t2.getText();
-			long contact= Long.parseLong(t3.getText());
+			String name = t2.getText();
+			long contact = Long.parseLong(t3.getText());
 			String address = t4.getText();
-			System.out.println("id : "+id);
-			System.out.println("name : "+name);
-			System.out.println("contact: "+contact);
-			System.out.println("address : "+address);
+			System.out.println("id : " + id);
+			System.out.println("name : " + name);
+			System.out.println("contact: " + contact);
+			System.out.println("address : " + address);
 			try {
 				Connection conn = SwingDemo.createConnection();
-				String url ="insert into aaa(id,name,contact,address) values(?,?,?,?)";
+				String url = "insert into aaa(id,name,contact,address) values(?,?,?,?)";
 				PreparedStatement pst = conn.prepareStatement(url);
 				pst.setInt(1, id);
 				pst.setString(2, name);
 				pst.setLong(3, contact);
 				pst.setString(4, address);
-				//DML->insert,update,delete => executeUpdate
-				//DQL->select =>executeQuery
+				// DML->insert,update,delete => executeUpdate
+				// DQL->select =>executeQuery
 				pst.executeUpdate();
 				System.out.println("data inserted");
 				t1.setText("");
@@ -120,25 +133,25 @@ class SwingDemo implements ActionListener {
 			} catch (Exception e2) {
 				e2.printStackTrace();
 			}
-		} 
-		
+		}
+
 		else if (e.getSource() == b2) {
 			System.out.println("search button clicked");
 			int id = Integer.parseInt(t1.getText());
 			try {
 				Connection conn = SwingDemo.createConnection();
-				String url ="select * from aaa where id=?";
+				String url = "select * from aaa where id=?";
 				PreparedStatement pst = conn.prepareStatement(url);
 				pst.setInt(1, id);
 				ResultSet rs = pst.executeQuery();
-				if(rs.next()) {
+				if (rs.next()) {
 					t1.setText(String.valueOf(rs.getInt("id")));
 					t2.setText(rs.getString("name"));
 					t3.setText(String.valueOf(rs.getLong("contact")));
 					t4.setText(rs.getString("address"));
-				}
-				else {
+				} else {
 					System.out.println("data not found");
+					new DataNotFound();
 					t1.setText("");
 					t2.setText("");
 					t3.setText("");
@@ -147,14 +160,50 @@ class SwingDemo implements ActionListener {
 			} catch (Exception e2) {
 				// TODO: handle exception
 			}
-		} 
-		
+		}
+
 		else if (e.getSource() == b3) {
 			System.out.println("update button clicked");
-		} 
-		
+			int id = Integer.parseInt(t1.getText());
+			String name = t2.getText();
+			long contact = Long.parseLong(t3.getText());
+			String address = t4.getText();
+			try {
+				Connection conn = SwingDemo.createConnection();
+				String url = "update aaa set name=?,contact=?,address=? where id=?";
+				PreparedStatement pst = conn.prepareStatement(url);
+				pst.setString(1, name);
+				pst.setLong(2, contact);
+				pst.setString(3, address);
+				pst.setInt(4, id);
+				pst.executeUpdate();
+				System.out.println("data updated");
+				t1.setText("");
+				t2.setText("");
+				t3.setText("");
+				t4.setText("");
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+
 		else if (e.getSource() == b4) {
 			System.out.println("delete button clicked");
+			int id = Integer.parseInt(t1.getText());
+			try {
+				Connection conn = SwingDemo.createConnection();
+				String url = "delete from aaa where id=?";
+				PreparedStatement pst = conn.prepareStatement(url);
+				pst.setInt(1, id);
+				pst.executeUpdate();
+				System.out.println("data deleted");
+				t1.setText("");
+				t2.setText("");
+				t3.setText("");
+				t4.setText("");
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
 		}
 
 	}
